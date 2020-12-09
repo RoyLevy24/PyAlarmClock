1412aaa17685ba4da9e1e170c71ebd06695b290e
Roy Levy <roy4@post.bgu.ac.il>
2020-12-09 08:47:26 +0200

    docs for PyAlarmClock

--- src/PyAlarmClock.py
+++ src/PyAlarmClock.py
@@ -1,24 +1,24 @@
+import argparse
 import os
-os.environ["KIVY_NO_ARGS"] = "1"
 import sys
 
-sys.path.append(".")
+# tells the os to ignore Kivy's argument parser
+os.environ["KIVY_NO_ARGS"] = "1"
+
+sys.path.append("..")
 sys.path.append("./src/")
 sys.path.append("./src/frontend/")
 sys.path.append("./src/backend/")
 sys.path.append("./src/service/")
-sys.path.append("./src/backend/OpenEyesDetection/")
 
-
-import argparse
 from frontend.frontend import *
-
 
 
 class PyAlarmClock():
     """
     This class is the main entry point of the app.
     """
+
     def __init__(self, args):
         # creates KivyMD alarm clock app
         self.app = AlarmApp(args)
@@ -29,14 +29,22 @@ class PyAlarmClock():
 
 
 def get_command_line_args():
+    """
+    Returns the application arguments parser.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--camera_num', type=int, default=0, help="camera device number for open eyes recognition")
-    parser.add_argument('-e', '--ear', type=float, default=0.31, help="eye aspect ratio for open eyes recognition")
-    parser.add_argument('-m', '--microphone_num', type=int, default=0, help="microphone device number for speech recognition")
-    parser.add_argument('-s', '--sim_thresh', type=float, default=0.65, help="similarity ratio between words for speech recognition")
+    parser.add_argument('-c', '--camera_num', type=int, default=0,
+                        help="camera device number for open eyes recognition")
+    parser.add_argument('-e', '--ear', type=float, default=0.31,
+                        help="eye aspect ratio for open eyes recognition")
+    parser.add_argument('-m', '--microphone_num', type=int, default=0,
+                        help="microphone device number for speech recognition")
+    parser.add_argument('-s', '--sim_thresh', type=float, default=0.65,
+                        help="similarity ratio between words for speech recognition")
     args = parser.parse_args()
 
     return args
+
 
 if __name__ == '__main__':
     args = get_command_line_args()
