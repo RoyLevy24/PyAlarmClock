7b4b6cf4e7eb1c12444374c95b7403a8810c2b0e
Roy Levy <roy4@post.bgu.ac.il>
2020-12-09 10:51:02 +0200

    docs alarm_active_screen dismiss_speech screen

--- src/frontend/screens/DismissSpeechScreen.py
+++ src/frontend/screens/DismissSpeechScreen.py
@@ -6,22 +6,20 @@ class DismissSpeechScreen(Screen, FloatLayout):
 
     def __init__(self, **kwargs):
         super(DismissSpeechScreen, self).__init__(**kwargs)
-        # self.speech_button_enabled_color = [0.807843137254902, 0.5764705882352941, 0.8470588235294118, 1.0]
-        # self.speech_button_disabled_color = [0.0, 0.0, 0.0, 0.12]
 
     def disable_speech_button(self):
         self.dismiss_speech_record.disabled = True
-        # self.dismiss_speech_record.md_bg_color = self.speech_button_disabled_color
 
     def enable_speech_button(self):
         self.dismiss_speech_record.disabled = False
-        # self.dismiss_speech_record.md_bg_color = self.speech_button_enabled_color
 
     def is_speech_button_enabled(self):
         return self.dismiss_speech_record.disabled == False
 
-    
     def show_speech_fail_snackbar(self):
+        """
+        Shows snackbar that indicates the user failed to speak a word.
+        """
         snackbar = Snackbar(text="Try Saying The Word Again")
         snackbar.show()
 
--- src/junk.py
+++ src/junk.py
@@ -18,3 +18,9 @@ module for experiments
         #         alarm_ringtone_proc.terminate()
         #         dismiss_func()
         #     return dismiss
+
+
+
+# color for record button
+# self.speech_button_enabled_color = [0.807843137254902, 0.5764705882352941, 0.8470588235294118, 1.0]
+# self.speech_button_disabled_color = [0.0, 0.0, 0.0, 0.12]
